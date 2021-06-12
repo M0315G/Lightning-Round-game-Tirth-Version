6aa7899cd3ee645311510eb71d59c4ee2b48bd13
M0315G <manish15gupta03@gmail.com>
2021-06-12 23:18:46 +0530

    Java Script file

---
+++ script.js
@@ -0,0 +1,159 @@
+var frbt1 = document.querySelector("#fr-btn-1");
+var fobt1 = document.querySelector("#fo-btn-1");
+var tibt1 = document.querySelector("#ti-btn-1");
+var nrbt1 = document.querySelector("#nr-btn-1");
+
+var frbt2 = document.querySelector("#fr-btn-2");
+var fobt2 = document.querySelector("#fo-btn-2");
+var tibt2 = document.querySelector("#ti-btn-2");
+var nrbt2 = document.querySelector("#nr-btn-2");
+
+var frbt3 = document.querySelector("#fr-btn-3");
+var fobt3 = document.querySelector("#fo-btn-3");
+var tibt3 = document.querySelector("#ti-btn-3");
+var nrbt3 = document.querySelector("#nr-btn-3");
+
+var frbt4 = document.querySelector("#fr-btn-4");
+var fobt4 = document.querySelector("#fo-btn-4");
+var tibt4 = document.querySelector("#ti-btn-4");
+var nrbt4 = document.querySelector("#nr-btn-4");
+
+var frbt5 = document.querySelector("#fr-btn-5");
+var fobt5 = document.querySelector("#fo-btn-5");
+var tibt5 = document.querySelector("#ti-btn-5");
+var nrbt5 = document.querySelector("#nr-btn-5");
+
+var frbt6 = document.querySelector("#fr-btn-6");
+var fobt6 = document.querySelector("#fo-btn-6");
+var tibt6 = document.querySelector("#ti-btn-6");
+var nrbt6 = document.querySelector("#nr-btn-6");
+
+var frbt7 = document.querySelector("#fr-btn-7");
+var fobt7 = document.querySelector("#fo-btn-7");
+var tibt7 = document.querySelector("#ti-btn-7");
+var nrbt7 = document.querySelector("#nr-btn-7");
+
+var frbt8 = document.querySelector("#fr-btn-8");
+var fobt8 = document.querySelector("#fo-btn-8");
+var tibt8 = document.querySelector("#ti-btn-8");
+var nrbt8 = document.querySelector("#nr-btn-8");
+
+
+// Set 1 ------------------------------------
+frbt1.addEventListener("click", () => {
+    frbt1.innerText = "In which city is Friends set?";
+});
+fobt1.addEventListener("click", () => {
+    fobt1.innerText = "Name 5 essential food items that Vansh brings everyday.";
+});
+tibt1.addEventListener("click", () => {
+    tibt1.innerText = "3 activities Tirth does on daily basis that starts with b";
+});
+nrbt1.addEventListener("click", () => {
+    nrbt1.innerText = "Whose proxy does Tirth make the most?";
+});
+
+
+// Set 2 ------------------------------------
+frbt2.addEventListener("click", () => {
+    frbt2.innerText = "What's the name of the coffee shop used by the characters?";
+});
+fobt2.addEventListener("click", () => {
+    fobt2.innerText = "What's the price of SamosaPav?";
+});
+tibt2.addEventListener("click", () => {
+    tibt2.innerText = "Where does Tirth spend the most time in the hostel? Other than room…";
+});
+nrbt2.addEventListener("click", () => {
+    nrbt2.innerText = "Favourite place of Tirth in Nirma?";
+});
+
+
+// Set 3 ------------------------------------
+frbt3.addEventListener("click", () => {
+    frbt3.innerText = "What hangs on Monica's purple door?";
+});
+fobt3.addEventListener("click", () => {
+    fobt3.innerText = "Tirth law me ladkiyo ko tadne jata thha!    [True False]";
+});
+tibt3.addEventListener("click", () => {
+    tibt3.innerText = "Last, when was Tirth emoo! [ hints:  he watched suits, the lion, familyman2]";
+});
+nrbt3.addEventListener("click", () => {
+    nrbt3.innerText = "The place of Nirma Tirth must have not been visited in this ( 1.5 + 1.5j ) years";
+});
+
+
+// Set 4 ------------------------------------
+frbt4.addEventListener("click", () => {
+    frbt4.innerText = "What is the name of Phoebe's alter-ego? ";
+});
+fobt4.addEventListener("click", () => {
+    fobt4.innerText = "K canteen average daily  kamai due to coupons they give us back which we never use.";
+});
+tibt4.addEventListener("click", () => {
+    tibt4.innerText = "How often does Tirth fart?";
+});
+nrbt4.addEventListener("click", () => {
+    nrbt4.innerText = "5 things Nirma showed us in orientation that were actually true!!!";
+});
+
+
+// Set 5 ------------------------------------
+frbt5.addEventListener("click", () => {
+    frbt5.innerText = "Who is Chandler and Joey's TV magazine addressed to?";
+});
+fobt5.addEventListener("click", () => {
+    fobt5.innerText = "Fastest Finger First : name 4 canteens of nirma with their one  most popular dish.";
+});
+tibt5.addEventListener("click", () => {
+    tibt5.innerText = " Number of exams Tirth gave without chEATing";
+});
+nrbt5.addEventListener("click", () => {
+    nrbt5.innerText = "who screamed and applauded the tirth and sunidhi’s song in vaudeville.?";
+});
+
+
+// Set 6 ------------------------------------
+frbt6.addEventListener("click", () => {
+    frbt6.innerText = "What is Janice most likely to say?";
+});
+fobt6.addEventListener("click", () => {
+    fobt6.innerText = "Eating 4 packets of what are the real victory";
+});
+tibt6.addEventListener("click", () => {
+    tibt6.innerText = "Describe Tirth in 3 words";
+});
+nrbt6.addEventListener("click", () => {
+    nrbt6.innerText = "Favorite food of k canteen";
+});
+
+
+// Set 7 ------------------------------------
+frbt7.addEventListener("click", () => {
+    frbt7.innerText = "What is Chandler's middle name?";
+});
+fobt7.addEventListener("click", () => {
+    fobt7.innerText = "Which drink complete’s tirth’s every food?";
+});
+tibt7.addEventListener("click", () => {
+    tibt7.innerText = "What is Tirths favorite sport to play?";
+});
+nrbt7.addEventListener("click", () => {
+    nrbt7.innerText = "Tirths favorite teacher";
+});
+
+
+// Set 8 ------------------------------------
+frbt8.addEventListener("click", () => {
+    frbt8.innerText = "Where does Ross work as a professor in season 6?";
+});
+fobt8.addEventListener("click", () => {
+    fobt8.innerText = "What is the price of dark chocolate cone?";
+});
+tibt8.addEventListener("click", () => {
+    tibt8.innerText = "Tirth favorite food cuisine";
+});
+nrbt8.addEventListener("click", () => {
+    nrbt8.innerText = "How many club did tirth work in (1 event also works)";
+});
